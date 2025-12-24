4c9ad206abaa56ee051ee5fb49506bfdfe127672
SakiiCode <13037299+SakiiCode@users.noreply.github.com>
2025-12-24 02:51:01 +0100

    update svd2rust and 2024 edition

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-svd2rust -i LPC178x7x-mod.svd --target cortex-m --reexport-interrupt
+svd2rust -i LPC178x7x-mod.svd --target cortex-m --reexport-interrupt --edition=2024
 rm -rf src
 form -i lib.rs -o src/ && rm lib.rs
 cargo fmt
